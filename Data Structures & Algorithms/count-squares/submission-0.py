ad2407f04cbe4912d1c55996bca4086c172900b7
Gourav Verma <gouravv.contact@gmail.com>
2026-08-04 18:16:40 +0530

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.py
@@ -0,0 +1,18 @@
+class CountSquares:
+
+    def __init__(self):
+        self.pts_count = defaultdict(int)
+        self.pts = []
+
+    def add(self, point: List[int]) -> None:
+        self.pts_count[tuple(point)] += 1
+        self.pts.append(point)
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+        for x, y in self.pts:
+            if (abs(py - y) != abs(px - x) or x == px or y == py):
+                continue
+            res += self.pts_count[(x, py)] * self.pts_count[(px, y)]
+        return res
