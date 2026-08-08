deccfc377a073ef5df257bbd67e73d616970dd8f
Gourav Verma <gouravv.contact@gmail.com>
2026-08-08 14:55:44 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        
+        pivot = nums[len(nums) // 2]
+        left = [x for x in nums if x < pivot]
+        middle = [x for x in nums if x == pivot]
+        right = [x for x in nums if x > pivot]
+
+        return self.sortArray(left) + middle + self.sortArray(right)
